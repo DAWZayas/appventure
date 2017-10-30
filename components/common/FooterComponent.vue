e0487895799a064a43852173815e0f3fc396d8f3
Juanjo <smrjuanjomovil@gmail.com>
2017-10-30 12:12:34 +0100

    Added images and edited footer

--- components/common/FooterComponent.vue
+++ components/common/FooterComponent.vue
@@ -2,14 +2,24 @@
   <footer class="footer-grid">
   <div class="copy">&copy Soy Programador S.L.</div>
     <div class="social">
-      <a href="https://www.facebook.com/"><img class="social-img" src="images/social/facebook.png" alt="facebook"></a>
-      <a href="https://www.twitter.com/"><img class="social-img" src="images/social/twitter.png" alt="twitter"></a>
-      <a href="https://www.instagram.com/"><img class="social-img" src="images/social/instagram.png" alt="instagram"></a>
-      <a href="https://plus.google.com/"><img class="social-img" src="images/social/google-plus.png" alt="google-plus"></a>
+      <a href="https://www.facebook.com/"><img class="social-img" src="facebook" alt="facebook"></a>
+      <a href="https://www.twitter.com/"><img class="social-img" src="twitter" alt="twitter"></a>
+      <a href="https://www.instagram.com/"><img class="social-img" src="instagram" alt="instagram"></a>
+      <a href="https://plus.google.com/"><img class="social-img" src="googlePLus" alt="google-plus"></a>
     </div>
   </footer>
 </template>
 <script>
+  export default {
+    data () {
+      return {
+        facebook: require('../../assets/images/social/facebook.png'),
+        twitter: require('../../assets/images/social/twitter.png'),
+        instagram: require('../../assets/images/social/instagram.png'),
+        googlePLus: require('../../assets/images/social/google-plus.png')
+      }
+    }
+  }
 </script>
 <style lang="css">
     footer {
