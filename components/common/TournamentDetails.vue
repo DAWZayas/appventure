d807cd6c1c33fac14436e0472d2568f11157684c
JuanJo <smrjuanjomovil@gmail.com>
2017-11-29 12:45:01 +0100

    added card and style of tournamentdetails

--- components/common/TournamentDetails.vue
+++ components/common/TournamentDetails.vue
@@ -1,19 +1,34 @@
 <template>
-<div>
-  <div id="carousel" class="carousel slide" data-ride="carousel" v-touch="{ right: () => swipe('#prev'), left: () => swipe('#next') }">
-    <div class="carousel-inner" role="listbox">
-      <div v-for="(item, i) in items" :key="i" :class="item.class" class="carousel-item">
-          <img class="d-block w-100 aaa" :src="item.src" :alt="item.alt">
+  <div>
+    <div class="container-box">
+      <div id="carousel" class="carousel slide" data-ride="carousel" v-touch="{ right: () => swipe('#prev'), left: () => swipe('#next') }">
+        <div class="carousel-inner" role="listbox">
+          <div v-for="(item, i) in items" :key="i" :class="item.class" class="carousel-item">
+              <img class="d-block w-100 aaa" :src="item.src" :alt="item.alt">
+          </div>
+          <a id="prev" class="carousel-control-prev" href="#carousel" role="button" data-slide="prev"></a>
+          <a id="next" class="carousel-control-next" href="#carousel" role="button" data-slide="next"></a>
+        </div>
       </div>
-      <a id="prev" class="carousel-control-prev" href="#carousel" role="button" data-slide="prev"></a>
-      <a id="next" class="carousel-control-next" href="#carousel" role="button" data-slide="next"></a>
+      <section class="main-data section-style">
+        <h1>{{ tournament.strong }}</h1>
+        <p>{{ tournament.location }}</p>
+      </section>
+      <section class="description p-1">
+        <v-card color="white darken-2" class="black--text">
+          <v-card-title primary-title>
+            <div class="headline">Una pequeña descripción</div>
+            <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
+          </v-card-title>
+        </v-card>
+      </section>
+      <section class="section-style">
+        <h6>Información practica</h6>
+        
+
+      </section>
     </div>
   </div>
-  <div class="main-data">
-    <h1>{{ tournament.strong }}</h1>
-    <p>{{ tournament.location }}</p>
-  </div>
-</div>
 </template>
 <script>
 export default {
@@ -45,17 +60,33 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
+  * + * {
+    margin-top: 0.60em
+  }
+  .container-box {
+    max-width: 100%;
+    padding: 0;
+    background-color: #E8E8E8;
+  }
   .aaa {
     margin-top: -2em;
   }
   #carousel {
     height: 25vh;
   }
-  .main-data {
-    margin-top: 0.25em;
+  .section-style {
+    background-color: #FFFFFF;
+    padding: .25em;
+
+    &.main-data {
+      margin-top: 0;
+    }
   }
   .practical-information {
     width: 90%;
+  }
+  p {
+    margin: 0
   }
 </style>
