8cf6c3c280a2145c3be2f4250d610c5244a0a1ad
juanhosk <smrjuanjomovil@gmail.com>
2018-01-08 09:26:14 +0100

    added authentification with google and facebook

--- components/landing/LogInComponent.vue
+++ components/landing/LogInComponent.vue
@@ -25,12 +25,38 @@
       >
         Log In
       </v-btn>
+
+      <div class="d-flex justify-content-between align-items-center">
+        <div>
+          <hr>
+        </div>
+        <div>
+          <v-btn
+            @click="loginWithFacebook"
+          >
+          <img :src="this.imgFacebook">
+          </v-btn>
+        </div>        
+        <div style="max-width: 1em"> O </div>
+        <div>
+          <v-btn
+            @click="loginWithGoogle"
+          >
+          <img :src="this.imgGoogle">
+          </v-btn>
+        </div>
+        <div>
+          <hr>
+        </div>
+      </div>
       <p class="home-forgot"><a href="#">Forgot Password?</a></p>
     </v-form>
   </div>
 </template>
 <script>
   import { mapActions } from 'vuex'
+  import firebase from 'firebase';
+  import firebaseApp from '~/firebaseapp';
 
   export default {
     data () {
@@ -38,9 +64,11 @@
         valid: true,
         email: '',
         password: '',
+        imgFacebook: require('../../assets/images/social/facebook.png'),
+        imgGoogle: require('../../assets/images/social/google-plus.png'),
         emailRules: [
           (v) => !!v || 'E-mail is required',
-          (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+          (v) => /^\w([\.-]?\w)*@\w([\.-]?\w)*(\.\w{2,3})$/.test(v) || 'E-mail must be valid'
         ],
         passRules: [
           (v) => !!v || 'Name is required',
@@ -54,6 +82,20 @@
         if (this.$refs.form.validate()) {
           this.authenticate({email: this.email, password: this.password})
         }
+      },
+      loginWithFacebook(){
+        let provider = new firebase.auth.FacebookAuthProvider();
+        firebaseApp.auth().signInWithPopup(provider).then(function(result) {
+          console.log(result)
+        }).catch(function(error) {
+          console.log(error)
+        });
+      },
+      loginWithGoogle(){
+        firebaseApp.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+          firebaseApp.auth().getRedirectResult().catch(function(error) {
+            this.error = error;
+        }); 
       }
     }
   }
