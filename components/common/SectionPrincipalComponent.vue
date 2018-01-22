17855a18e09517513a6bd09113fb2f0ac8b1f122
Garlos <gaarlos@users.noreply.github.com>
2018-01-22 20:59:26 +0100

    LoadingComponent finished
    FIxed SectionPrincipal too

--- components/common/SectionPrincipalComponent.vue
+++ components/common/SectionPrincipalComponent.vue
@@ -2,7 +2,7 @@
   <section>
     <h5>Torneos cerca de ti</h5>
     <section class="d-flex slide">
-      <article-tournament-component v-for="(tournament , key) in tournamentsDisplay" :key="key" :tournament="tournament" :id="key"></article-tournament-component>
+      <article-tournament-component v-for="(tournament, key) in tournamentsDisplay" :key="key" :tournament="tournament" :id="key"></article-tournament-component>
       <nuxt-link to="/moreTournaments"><show-more-component></show-more-component></nuxt-link>
     </section>
   </section>
@@ -21,7 +21,7 @@
       tournamentsDisplay: function () {
         const xs = []
         for (var i in this.tournaments) {
-          if (xs.lenght < 8) {
+          if (xs.length < 8) {
             xs.push(this.tournaments[i])
           }
         }
---
+++ components/layoutComponents/loadingComponent.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="loader-container">
+    <div class="d-flex flex-column align-items-center">
+      <img :src="logo" class="load-img" alt="AppVenture">
+    </div>
+    <div class="load-t">
+      <span>{{ loadingT }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'login',
+  data () {
+    return {
+      logo: require('~/assets/logo-animated.svg'),
+      loadingT: 'Cargando'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @keyframes loader {
+    0%, 100% { max-width: 5rem; }
+    25%, 75% { max-width: 15rem }
+
+    0% { transform: rotate(0); }
+    25% { transform: rotate(45deg); }
+    75% { transform: rotate(270deg); }
+    100% { transform: rotate(360deg); }
+  }
+
+  @keyframes dots {
+    0%, 100% { width: 0; }
+    20% { width: 0; }
+    50% { width: 2.5rem; }
+  }
+
+  .loader-container {
+    position: relative;
+    overflow: hidden;
+    background-color: rgba(19, 35, 47, 0.9);
+    height: 100%;
+  
+  }
+
+  .load-img {
+    position: absolute;
+    top: 25%;
+    animation-timing-function: cubic-bezier(.90,.15,.15,.90);
+    animation-name: loader;
+    animation-duration: 1.5s;
+    animation-iteration-count: infinite;
+    animation-fill-mode: both;
+  }
+
+  .load-t {
+    position: absolute;
+    font-size: 2.5rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgb(200, 200, 200);
+    width: 100%;
+    top: 75%;
+    text-align: center;
+
+    & span:after {
+      position: absolute;
+      overflow: hidden;
+      animation: dots 2s infinite;
+      content: "\2026";
+    }
+  }
+</style>
