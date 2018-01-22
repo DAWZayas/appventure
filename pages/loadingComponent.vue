9dfe6fb5e578de486afcd531a87ec6d7d8fc924f
gaarlos <garlos.daw@gmail.com>
2018-01-22 14:07:39 +0100

    LoadingComponent in progress

---
+++ pages/loadingComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="d-flex loader flex-column align-items-center">
+    <img :src="logo" class="loadr" alt="AppVenture">
+  </div>
+</template>
+<script>
+export default {
+  layout: 'login',
+  data () {
+    return {
+      logo: require('~/assets/logo.svg'),
+      isSignUp: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @keyframes loader {
+    0% {
+      max-width: 10em;
+      transform: rotate(0);
+    }
+
+    50% {
+      max-width: 20em;
+      transform: rotate(45deg);
+    }
+
+    100% {
+      max-width: 10em;
+      transform: rotate(0);
+    }
+  }
+
+  .loader {
+    background-color: rgba(19, 35, 47, 0.9);
+    height: 100vh;
+
+    & > .loadr {
+      animation-timing-function: cubic-bezier(.90,.15,.15,.90);
+      animation-name: loader;
+      animation-duration: 1.5s;
+      animation-iteration-count: infinite;
+
+    }
+  }
+</style>
