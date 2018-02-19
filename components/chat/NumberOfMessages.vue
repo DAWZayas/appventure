9e00a24fd4352e19bc9f287fa79bcfc1970498cc
juanhosk <smrjuanjomovil@gmail.com>
2018-02-19 12:35:25 +0100

    New functionality in chat admin

---
+++ components/chat/NumberOfMessages.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-chip color="secondary" small selected>
+    {{ numberOfMessage }}
+  </v-chip>
+</template>
+<script>
+export default {
+  props: ['user'],
+  computed: {
+    numberOfMessage () {
+      var count = 0
+      for (var i in this.user.messages) {
+        count++
+        console.log(i)
+      }
+      return count
+    }
+  }
+}
+</script>
