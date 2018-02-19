3ce1532351a47463f148ccd64ec27d523d156ace
juanhosk <smrjuanjomovil@gmail.com>
2018-02-19 17:09:02 +0100

    Minnor Changes

--- components/chat/Chat.vue
+++ components/chat/Chat.vue
@@ -7,7 +7,7 @@
             <img src="http://dhpsolucionesonline.com/mediapool/143/1431874/resources/big_31817754_0_350-350.jpg" alt="avatar">
           </v-list-tile-avatar>
           <v-list-tile-content style="width: 100%">
-            <v-list-tile-title> Hola, ¿ en que podemos ayudarle ? </v-list-tile-title>
+            <span> Hola, ¿En que podemos ayudarle? </span>
           </v-list-tile-content>
         </div>
         <v-divider class="m-0"></v-divider>
@@ -16,8 +16,8 @@
     <v-list two-linev class="mx-2 pb-0 pt- px-2">
       <template  v-for="(message, index, key) in this.userData.type === 'Venture' ? this.userData.messages : users[id].messages"  :message="message">
         <div :key="index" class="d-flex my-3 align-items-center">
-          <v-list-tile-content style="width: 100%">
-            <v-list-tile-title v-html="message.message"></v-list-tile-title>
+          <v-list-tile-content style="width: 100%;">
+            <span>{{message.message}}</span>
           </v-list-tile-content>
           <v-list-tile-avatar>
             <img :src="users[message.issuing].photoURL" alt="avatar">
@@ -26,14 +26,16 @@
         <v-divider class="m-0" style="width: calc( 100% - 60px )" :key="key"></v-divider>
       </template>
     </v-list>
-    <div class="kk m-2 d-flex">
+    <div class="send p-2 d-flex">
       <v-text-field
-        name="input-1"
+        name="input-7-1"
         label="Mensaje"
         v-model="newMessage"
         @keyup.enter="pushMessage"
+        multi-line
+        rows="2"
       ></v-text-field>
-      <v-btn @click="pushMessage">Enviar</v-btn>
+      <v-btn small fab color="primary" @click="pushMessage"><v-icon >send</v-icon></v-btn>
     </div>
   </div>
 </template>
@@ -60,9 +62,11 @@
   }
 </script>
 <style>
-  .kk {
+  .send {
     position: fixed ;
     bottom: 2em;
+    width: 100%;
+     align-items: center;
   }
 </style>
 
