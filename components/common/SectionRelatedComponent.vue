2f6a686023841000a05293d3c008bf9a66a926b8
juanhosk <smrjuanjomovil@gmail.com>
2018-02-20 16:34:43 +0100

    Section related finished!

--- components/common/SectionRelatedComponent.vue
+++ components/common/SectionRelatedComponent.vue
@@ -17,9 +17,34 @@
       ShowMoreComponent
     },
     computed: {
-      ...mapGetters({ tournaments: 'getArrayTournaments' }),
-      tournamentsDisplay: function () {
-        return this.tournaments.slice(0, 8)
+      ...mapGetters({ tournaments: 'getArrayTournaments', participating: 'getParticipating' }),
+      tournamentsDisplay () {
+        var xs = []
+        var count = { motor: 0, cardgames: 0, sports: 0, esports: 0 }
+        var max = 0
+        var max2 = 0
+        var maxCategory
+        var maxCategory2
+        var size = 0
+
+        for (let i in this.participating) {
+          this.participating[i] === 'motor' ? count.motor++ : this.participating[i] === 'cardgames' ? count.cardgames++ : this.participating[i] === 'sports' ? count.sports++ : count.esports++
+          size++
+        }
+        if (size) {
+          for (let i in count) {
+            count[i] > max ? (max = count[i], maxCategory = i) : null
+          }
+          for (let i in count) {
+            count[i] > max2 && count[i] < max ? (max2 = count[i], maxCategory2 = i) : null
+          }
+          for (let i = 0; i < this.tournaments.length; i++) {
+            this.tournaments[i].category === maxCategory || this.tournaments[i].category === maxCategory2 ? xs.push(this.tournaments[i]) : null
+          }
+          return xs.slice(0, 8)
+        } else {
+          return this.tournaments
+        }
       }
     }
   }
