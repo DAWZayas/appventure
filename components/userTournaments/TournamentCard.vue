766b7ff73c4ae60b8c87b9b42d8542f0a278c43a
Garlos <gaarlos@users.noreply.github.com>
2018-02-25 18:08:23 +0100

    Styled MyTournaments Card

--- components/userTournaments/TournamentCard.vue
+++ components/userTournaments/TournamentCard.vue
@@ -1,21 +1,17 @@
 <template>
-  <v-layout row wrap m-2>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <card-image-component :src="src"></card-image-component>
-        <v-card-title>
-          <div>
-            <p>{{tournament.name}}</p>
-            <span class="grey--text">Deporte: {{tournament.subcategory}}</span><br>
-            <span class="grey--text">Fecha: {{tournament.initDate}}</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">Ver resultado</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-card class="square m-1">
+    <card-image-component :src="src"></card-image-component>
+    <v-card-title>
+      <div>
+        <p>{{tournament.name}}</p>
+        <span class="grey--text">Deporte: {{tournament.subcategory}}</span><br>
+        <span class="grey--text">Fecha: {{tournament.initDate}}</span>
+      </div>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn flat color="orange">Ver resultado</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
   import { CardImageComponent } from '~/components/common/tournaments'
